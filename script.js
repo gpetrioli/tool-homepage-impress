fc5d4659a93f6850df1f6db6269f2bbe900acf34
Gabriele Petrioli <gpetrioli@users.noreply.github.com>
2017-12-20 18:35:33 +0200

    Add mousewheel progression through impress

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
-var impressApi = impress();
-impressApi.init();
+(function () {
+    'use strict';
+    let scrolling = false,
+        impressApi = impress();
+    
+    impressApi.init();
+    
+    document.documentElement.addEventListener('impress:stepenter', function (e) {
+       scrolling = false;
+    });
+    
+    document.documentElement.addEventListener('wheel', function (e) {
+        if (!scrolling && e.wheelDelta !== 0) {
+            if (e.wheelDelta < 0){
+                // wheel scroll down
+                impressApi.next();
+            } else if (e.wheelDelta > 0) {
+                // wheel scroll up
+                impressApi.prev();
+            }
+            scrolling = true;
+        }
+    });
+    
+    
+    // debug
+    // temporarily expose the impress API to the global
+    window['impressApi'] = impressApi;
+})();
